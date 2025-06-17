9867510ab2338de92a8d9827c4faf07831f3c285
Anton Katkov <katkov.anton@gmail.com>
2025-06-17 12:50:33 +0300

    v0.1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Пустой JS-файл для будущих функций 
+
+// Прокрутка галерей по горизонтали колесом мыши
+
+document.querySelectorAll('.gallery').forEach(gallery => {
+  gallery.addEventListener('wheel', (evt) => {
+    evt.preventDefault();
+    gallery.scrollLeft += evt.deltaY;
+  });
+}); 
